34e41a38821dc8c49b58c320f982f62d8393d5f9
Sukanya <sukanya1189@gmail.com>
2020-04-21 20:04:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
- res.send("hello Jenkins server");
+ res.send("hello Jenkins");
 });
 app.listen(process.env.PORT || 3000);
  
