0e493e83b603e46984e14da06f602a55265b911b
Sukanya <sukanya1189@gmail.com>
2020-06-30 16:34:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
 
- res.send("hello Jenkins world. This is web7");
+ res.send("hello Jenkins world. This is master commit");
 
 });
 app.listen(process.env.PORT || 3000);
