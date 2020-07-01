edc69b8458ee1f3479434a61e0764201c26823c2
Sukanya <sukanya1189@gmail.com>
2020-07-01 11:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
 
- res.send("test1 branch created from test branch for testing purpose");
+ res.send("test2 branch created from test1 branch for testing purpose");
 
 });
 app.listen(process.env.PORT || 3000);
